cb14e23b6d518cdacbb717fef601ff1c71599e15
qais dwairi <qaisdw96@hotmail.com>
2023-03-25 16:41:41 +0000

    test-13

--- server.js
+++ server.js
@@ -3,10 +3,8 @@
 const express = require('express');
 const axios = require("axios");
 require('dotenv').config()
-const cors = require("cors")
 const bodyParser = require('body-parser')
 const app = express()
-app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
