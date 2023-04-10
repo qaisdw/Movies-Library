34c694572e4c2e13ec356198f3c4bcf5cd393828
qaisdw <qaisdw96@hotmail.com>
2023-04-10 08:26:54 +0000

    movie

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 // to kill a port => sudo kill -9 $(sudo lsof -t -i:portnumber)
 const express = require('express');
+var cors = require('cors')
 require('dotenv').config()
 const axios = require("axios");
 const bodyParser = require('body-parser')
 const app = express()
+app.use(cors())
 const Api_Key=process.env.api_key;
 // parse application/json
 app.use(bodyParser.json())
